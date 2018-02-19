99df8d76b5b5ed0508ef723fb853587555b9a02d
David Toomey <d.toomey@temple.edu>
2018-02-18 23:50:50 -0500

    1st commit

---
+++ HorseRace.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.18;
+
+import './Ownable.sol';
+
+contract HorseRaceFactory is Ownable {
+    function HorseRaceFactory() public {
+        
+    }
+
+    function createRace(uint minStake) public {
+
+    }
+}
+
+contract HorseRace {
+    struct Race {
+
+    }
+}
