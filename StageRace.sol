813d22631d930b6f32c8ec189abf8e2542fc68cd
David Toomey <d.toomey@temple.edu>
2018-02-19 13:07:43 -0500

    added win/loss probabilities based on horse level to _startRace function

--- StageRace.sol
+++ StageRace.sol
@@ -12,7 +12,7 @@ contract TwoPlayerRace is Race, HorseRace, Ownable, HorseBase, HorseCore  {
 
     function TwoPlayerRace(string nameOfRace, uint wager) public {
         uint raceGrandPrize = wager*2;
-        _createRace(nameOfRace, 1 ether, 0, 0, RaceStatus.Started, raceGrandPrize, 0, 0);
+        _createRace(nameOfRace, 1 ether);
         racingPlayers = 0;
     }
 
@@ -27,6 +27,7 @@ contract TwoPlayerRace is Race, HorseRace, Ownable, HorseBase, HorseCore  {
     enum RaceStatus {
         Started,
         Finished,
+        Pending,
         Cancelled
     }
     
@@ -48,13 +49,13 @@ contract TwoPlayerRace is Race, HorseRace, Ownable, HorseBase, HorseCore  {
     
     function _createRace(
         string _raceName, 
-        uint _stake, 
-        uint _horseOneId, 
-        uint _horseTwoId,
-        RaceStatus _status,
-        uint _grandPrize,
-        uint _horseWinnerId,
-        uint _horseLoserId
+        uint _stake 
+        // uint _horseOneId, 
+        // uint _horseTwoId,
+        // RaceStatus _status,
+        // uint _grandPrize,
+        // uint _horseWinnerId,
+        // uint _horseLoserId
         
     )   internal 
         returns (uint) 
@@ -62,12 +63,12 @@ contract TwoPlayerRace is Race, HorseRace, Ownable, HorseBase, HorseCore  {
         Race memory _race = Race({
             raceName: _raceName,
             stake: _stake,
-            horseOneId: _horseOneId,
-            horseTwoId: _horseTwoId,
-            status: _status,
-            grandPrize: _grandPrize,
-            horseWinnerId: _horseWinnerId,
-            horseLoserId: _horseLoserId
+            horseOneId: 0,
+            horseTwoId: 0,
+            status: RaceStatus.Pending,
+            grandPrize: 0,
+            horseWinnerId: 0,
+            horseLoserId: 0
         });
         uint newRaceId = races.push(_race) - 1;
         
@@ -76,27 +77,58 @@ contract TwoPlayerRace is Race, HorseRace, Ownable, HorseBase, HorseCore  {
         return newRaceId;
     }
     
-    function _enterRace(uint enterRaceId, uint enterHorseId) public onlyOwner payable {
+    function _enterRace(uint enterRaceId, uint enterHorseId) public payable {
         require(msg.value > races[enterRaceId].stake);
+        require(HorseIndexToOwner[enterHorseId] == msg.sender);
         require(racingPlayers < 2);
+        
         if (races[enterRaceId].horseOneId == 0) {
             races[enterRaceId].horseOneId = enterHorseId;
         }
+        
         if (races[enterRaceId].horseTwoId == 0) {
             races[enterRaceId].horseTwoId = enterHorseId;
         }
+        
         racingPlayers++;
+        
         if (racingPlayers == 2) {
             uint setGrandPrize = msg.value*2;
             setGrandPrize = races[enterRaceId].grandPrize;
-            _startRace(enterRaceId, setGrandPrize, races[enterRaceId].horseOneId, races[enterRaceId].horseTwoId);
+            _startRace(enterRaceId, races[enterRaceId].horseOneId, races[enterRaceId].horseTwoId);
         }
-        // getHorse(enterHorseId);
+
     }
     
-    function _startRace(uint startRaceId, uint _setGrandPrize, uint hOneId, uint hTwoId) internal {
+    function _startRace(uint startRaceId, uint hOneId, uint hTwoId) internal {
         require(races[startRaceId].grandPrize > 0);
-        horses[hOneId];
+        RaceStage(startRaceId);
+        uint horseOneLevel = horses[hOneId].level;
+        uint horseTwoLevel = horses[hTwoId].level;
+        uint horseOneLevelDiff = horseOneLevel - horseTwoLevel;
+        uint horseTwoLevelDiff = horseTwoLevel - horseOneLevel;
+        uint setHorseOneWinProbability;
+        uint setHorseTwoWinProbability;
+        if (horseOneLevel == horseTwoLevel) {
+            setHorseOneWinProbability = 50;
+            setHorseTwoWinProbability = 50;
+        }
+        if (horseOneLevelDiff >= 5) {
+            setHorseOneWinProbability = 75;
+            setHorseTwoWinProbability = 25;
+        }
+        if (horseTwoLevelDiff >= 5) {
+            setHorseOneWinProbability = 25;
+            setHorseTwoWinProbability = 75;
+        }
+        if (horseOneLevelDiff <= 4) {
+            setHorseOneWinProbability = 60;
+            setHorseTwoWinProbability = 40;
+        }
+        if (horseTwoLevelDiff <= 4) {
+            setHorseOneWinProbability = 40;
+            setHorseTwoWinProbability = 60;
+        }
     }
 
     // function decideWinner(uint _raceId) internal {
